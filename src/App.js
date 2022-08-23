fa9230bc0ae17500a6f1ba43a87317c4e92addf7
Ashuu02 <ashushimpi02@gmail.com>
2022-08-24 02:01:50 +0530

    added main.jsx and navbar done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 function App() {
   return (
     <>
-      <Navbar/>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
-
+import React from 'react';
+import Main from '../components/Main';
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+    <Main/>
+    </>
   )
 }
 
