16de3244efd2baa8bc86fa7b03faa0a3337fad67
Ashuu02 <ashushimpi02@gmail.com>
2022-08-25 01:08:58 +0530

    react frontend done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
-import Main from '../components/Main';
+import React from "react";
+import Main from "../components/Main";
+import Row from "../components/Row";
+import requests from "../Requests";
 const Home = () => {
   return (
     <>
-    <Main/>
+      <Main />
+      <Row rowID='1' title='Upcoming' fetchUrl={requests.requestUpcoming}/>
+      <Row rowID='2' title='Popular' fetchUrl={requests.requestPopular}/>
+      <Row rowID='3' title='Trending' fetchUrl={requests.requestTrending}/>
+      <Row rowID='4' title='Top rated' fetchUrl={requests.requestTopRated}/>
+      <Row rowID='5' title='Horror' fetchUrl={requests.requestHorror}/>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
